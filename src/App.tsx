837063f8ad3e7cbf40e9547133f4ca36e6c0ef25
Chinmay Narayan <Chinmay@amuselabs.com>
2023-10-10 06:09:04 +0530

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          React app for smoothening out the school's workflow.
         </p>
         <a
           className="App-link"
